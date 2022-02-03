3e11fda6120802af10636659d35d5f337d02a16b
Shifra Wagshall <shifrawagshall@gmail.com>
2022-02-02 23:08:54 -0500

    Added basic fetch function to connect to the api. ]

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
 
+console.log("IN SCRIPT!!!!")
+
+var getLoc=document.getElementById('location-form');
+// var apiUrl='https://api.openweathermap.org/data/2.5/weather?q=' +cityName+ '&appid=3b55a267ab7fd7c8d9aeb4d81c487408'
+var cityName= document.getElementById('location')
+var apiUrl='https://api.openweathermap.org/data/2.5/weather?q=Miami&appid=3b55a267ab7fd7c8d9aeb4d81c487408'
+
+
+function getWeather(e, cityName){
+    e.preventDefault()
+    console.log("in weather function ")
+
+    fetch(apiUrl)
+    .then(function (res) {
+        if(res.ok){
+            console.log("res worked")
+            res.json()
+            .then(function (data) {
+                console.log(data)
+                displayWeather(data, cityName)
+        });
+        }
+        else{
+            console.log("res x worked")
+        }
+})
+}
+
+function displayWeather(data, cityName){
+    console.log("in display function")
+}
+
+
+
+
+getLoc.addEventListener('click', getWeather);
+console.log(apiUrl)
